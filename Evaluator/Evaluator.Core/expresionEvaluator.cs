c114abe311daf4fc3305ec2d9ab8818553b6ad22
gnmoiso <moisoyt@gmail.com>
2025-09-19 18:20:19 -0500

    Finish Basic Evaluator

--- Evaluator/Evaluator.Core/expresionEvaluator.cs
+++ Evaluator/Evaluator.Core/expresionEvaluator.cs
@@ -5,31 +5,53 @@ public class expresionEvaluator
     public static double Evaluate(string infix)
     {
         var postfix = InfixToPostfix(infix);
-
         return Calculate(postfix);
     }
 
-    private static object InfixToPostfix(string infix)
+    private static string InfixToPostfix(string infix)
     {
         var stack = new Stack<char>();
         var postfix = string.Empty;
-        foreach (var item in postfix)
+        foreach (var item in infix)
         {
             if (IsOperator(item))
             {
-                if (stack.Count > 0)
+                if (item == ')')
                 {
-
+                    do
+                    {
+                        postfix += stack.Pop();
+                    } while (stack.Peek() != '(');
+                    stack.Pop();
                 }
                 else
                 {
-                    stack.Push(item);
+                    if (stack.Count > 0)
+                    {
+                        if (PriorityInfix(item) > PriorityStack(stack.Peek()))
+                        {
+                            stack.Push(item);
+                        }
+                        else
+                        {
+                            postfix += stack.Pop();
+                            stack.Push(item);
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(item);
+                    }
                 }
             }
             else
             {
                 postfix += item;
             }
+        }
+        while (stack.Count > 0)
+        {
+            postfix += stack.Pop();
         }
         return postfix;
     }
@@ -54,8 +76,32 @@ public class expresionEvaluator
         _ => throw new Exception("Invalid expresion"),
     };
 
-    private static double Calculate(object postfix)
+    private static double Calculate(string postfix)
     {
-        throw new NotImplementedException();
+        var stack = new Stack<double>();
+        foreach (char item in postfix)
+        {
+            if (IsOperator(item))
+            {
+                var op2 = stack.Pop();
+                var op1 = stack.Pop();
+                stack.Push(Calculate(op1, item, op2));
+            }
+            else
+            {
+                stack.Push(Convert.ToDouble(item.ToString()));
+            }
+        }
+        return stack.Peek();
     }
+
+    private static double Calculate(double op1, char item, double op2) => item switch
+    {
+        '*' => op1 * op2,
+        '/' => op1 / op2,
+        '+' => op1 + op2,
+        '-' => op1 - op2,
+        '^' => Math.Pow(op1, op2),
+        _ => throw new Exception("Invalid expresion"),
+    };
 }
--- Evaluator/Evaluator.UI.Console/Program.cs
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -3,6 +3,14 @@
 Console.WriteLine("Hello Evaluator!");
 
 var evaluator = new expresionEvaluator();
-var infix = "1+2";
-var result = expresionEvaluator.Evaluate(infix);
-Console.WriteLine($"{infix} = {result}");
+var infix1 = "4*5/(4+6)";
+var result1 = expresionEvaluator.Evaluate(infix1);
+Console.WriteLine($"{infix1} = {result1}");
+
+var infix2 = "4*(5+6-(8/2^3)-7)-1";
+var result2 = expresionEvaluator.Evaluate(infix2);
+Console.WriteLine($"{infix2} = {result2}");
+
+var infix3 = "7^(1/2)";
+var result3 = expresionEvaluator.Evaluate(infix3);
+Console.WriteLine($"{infix3} = {result3}");
